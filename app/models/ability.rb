3f3b955eaaa82110aa0dbc2ac6aaa6e9696b4c12
Luis Perichon <luisperichon@gmail.com>
2014-08-27 13:59:16 -0300

    guest user in ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,8 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+  	user ||= User.new # guest user (not logged in)
+  	
     cannot :manage, :all
 
     if user.username == 'yael.barcesat' || user.username == 'mariela.dossena' || user.username == 'luis.perichon'
