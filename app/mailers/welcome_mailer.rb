2cf209312df434ffaab3e9286d06501b7238f570
Luis Perichon <luisperichon@gmail.com>
2014-09-10 11:28:35 -0300

    remove fnz email

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -1,6 +1,4 @@
 class WelcomeMailer < MandrillMailer::TemplateMailer	
-  default from: 'fnz@padm.am'
-
   def welcome(participant)
   	mandrill_mail template: 'welcome',
                   subject: "Bienvenido al XI DeRose Festival de Buenos Aires",
