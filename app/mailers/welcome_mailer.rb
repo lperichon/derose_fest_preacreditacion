9e7f30bdb75cda9b420e204b2f03f2498c51abb2
Luis Perichon <luisperichon@gmail.com>
2014-09-10 11:35:29 -0300

    default from

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -1,4 +1,6 @@
 class WelcomeMailer < MandrillMailer::TemplateMailer	
+  default from: 'DeRose Festival BA <decana.ar@metododerose.org>'
+
   def welcome(participant)
   	mandrill_mail template: 'welcome',
                   subject: "Bienvenido al XI DeRose Festival de Buenos Aires",
