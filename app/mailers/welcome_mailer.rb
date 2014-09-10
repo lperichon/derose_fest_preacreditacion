9a2ad274b2dbcafd30b69b3d5a6e010adf806896
Luis Perichon <luisperichon@gmail.com>
2014-09-10 11:56:11 -0300

    dont use mandrill_mailer

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -1,12 +1,13 @@
-class WelcomeMailer < MandrillMailer::TemplateMailer	
-  default from: 'decana.ar@metododerose.org'
+class WelcomeMailer < ActionMailer::Base
+  default from: "decana.ar@metododerose.org"
 
   def welcome(participant)
-  	mandrill_mail template: 'welcome',
-                  subject: "Bienvenido al XI DeRose Festival de Buenos Aires",
-                  to: { email: participant.email, name: "#{participant.first_name} #{participant.last_name}" },
-                  vars: {
-                    'name' => participant.first_name
-                  }
+    mail to:      participant.email,
+         from:    "\"DeRose Festival BA\" <decana.ar@metododerose.org>",
+         subject: 'Bienvenido al XI DeRose Festival de Buenos Aires'
+    headers['X-MC-MergeVars'] = "{\"name\":\"#{participant.first_name}\"}" # variables
+    headers['X-MC-Template'] = "welcome"  # template
+    headers['X-MC-AutoText'] = 1 # generate text version
+    headers['X-MC-InlineCSS'] = "true" # inline css
   end
 end
