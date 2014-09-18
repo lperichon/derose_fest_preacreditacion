050f9be0f248ab4ba041f6967b0ecd7bb83bccb4
Luis Perichon <luisperichon@gmail.com>
2014-09-18 18:08:41 -0300

    reminder

---
+++ app/mailers/reminder_mailer.rb
@@ -0,0 +1,13 @@
+class ReminderMailer < ActionMailer::Base
+  default from: "decana.ar@metododerose.org"
+
+  def welcome(participant)
+    mail to:      participant.email,
+         from:    "\"DeRose Festival BA\" <decana.ar@metododerose.org>",
+         subject: 'Bienvenido al XI DeRose Festival de Buenos Aires'
+    headers['X-MC-MergeVars'] = "{\"name\":\"#{participant.first_name}\"}" # variables
+    headers['X-MC-Template'] = "recordatorio"  # template
+    headers['X-MC-AutoText'] = 1 # generate text version
+    headers['X-MC-InlineCSS'] = "true" # inline css
+  end
+end
