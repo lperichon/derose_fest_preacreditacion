925ab08f9eb1e02a866c7762cede4994c6918436
Luis Perichon <luisperichon@gmail.com>
2014-09-22 09:36:33 -0300

    encuesta

---
+++ app/mailers/survey_mailer.rb
@@ -0,0 +1,14 @@
+class SurveyMailer < ActionMailer::Base
+  default from: "decana.ar@metododerose.org"
+
+  def survey(participant)
+    mail to:      participant.email,
+         from:    "\"DeRose Festival BA\" <decana.ar@metododerose.org>",
+         subject: 'Encuesta de calidad - XI DeRose Festival de Buenos Aires'
+    headers['X-MC-MergeVars'] = "{\"name\":\"#{participant.first_name}\"}" # variables
+    headers['X-MC-Template'] = "encuesta
+    "  # template
+    headers['X-MC-AutoText'] = 1 # generate text version
+    headers['X-MC-InlineCSS'] = "true" # inline css
+  end
+end
