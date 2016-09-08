ef2292bbdb986dd9ccffe0b71f5a1da9c35c4e86
Luis Perichon <luisperichon@gmail.com>
2016-09-08 12:41:47 -0300

    remove attachment

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -2,8 +2,6 @@ class WelcomeMailer < ActionMailer::Base
   default from: "decana.ar@metododerose.org"
 
   def welcome(participant)
-    attachments['filename.jpg'] = File.read(Rails.public_path + 'ProgramaA3.jpg')
-    
     @participant = participant
 
     mail to:      participant.email,
