fe0abf1810eac8f06eaf230efa58602e76f25958
Luis Perichon <luisperichon@gmail.com>
2016-09-19 09:37:18 -0300

    survey

--- app/mailers/survey_mailer.rb
+++ app/mailers/survey_mailer.rb
@@ -2,6 +2,8 @@ class SurveyMailer < ActionMailer::Base
   default from: "decana.ar@metododerose.org"
 
   def survey(participant)
+  	@participant = participant
+  	
     mail to:      participant.email,
          from:    "\"DeRose Festival BA\" <decana.ar@metododerose.org>",
          subject: 'Encuesta de calidad - XIII DeRose Festival de Buenos Aires'
